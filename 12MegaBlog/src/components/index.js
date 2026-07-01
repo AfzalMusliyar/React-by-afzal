880f832c714968765a4f39b7352765d86d12fc91
AfzalMusliyar <afzalmuxliyar@gmail.com>
2026-07-02 01:28:47 +0530

    Add 12MegaBlog project

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,7 +2,8 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './Container/Container'
 import Logo from './Logo'
-import {LogoutBtn} from './Header/LogoutBtn'
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
 
 
-export {Header,Footer,Container,Logo,LogoutBtn};
+export {Header,Footer,Container,Logo,LogoutBtn,RTE};
